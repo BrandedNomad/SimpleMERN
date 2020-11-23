77aa8859295804c46f6be33726ac613b8544b17a
BrandedNomad <info@brandednomad.com>
2020-11-23 17:50:25 +0700

    feat: add login page

---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LoginCard from "./LoginCard";
+
+
+function LoginPage(){
+    return (
+        <div
+            className='loginPage-container'
+        >
+            <LoginCard/>
+
+        </div>
+    )
+}
+
+
+export default LoginPage;
